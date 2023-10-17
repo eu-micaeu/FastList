baa60495f36f570f0bc0e871809b8c023f92800e
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-10-17 18:00:15 -0300

    Continuando o curso de AngularJS.

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-data-biding',
   templateUrl: './data-biding.component.html',
   styleUrls: ['./data-biding.component.scss']
 })
-export class DataBidingComponent {
+export class DataBidingComponent implements OnInit{
 
   public nome: string = "Dener";
   public idade: number = 29;
@@ -13,4 +13,19 @@ export class DataBidingComponent {
   public checkedDisable: boolean = false;
   public imgSrc: string = "https://wallpapercave.com/wp/6K44j5E.jpg";
   public imgTitle: string = "Imagem qualquer";
+
+  public position: {x: number, y: number} = {x: 0, y: 0};
+
+  constructor() {}
+
+  ngOnInit(): void{}
+
+  public alertInfo(valor: MouseEvent){
+    console.log(valor);
+  }
+
+  public mouseMoveTest(valor: MouseEvent){
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
 }
